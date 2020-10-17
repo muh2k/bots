faf4b7d993ac58dc915da0f2f33a36805114e9b6
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 00:35:35 +0100

    deleted role ban perms

--- commands/ban.js
+++ commands/ban.js
@@ -2,12 +2,7 @@ module.exports = {
     name: 'ban',
     description: "this command will ban a user",
     execute(message, args){
-        if(message.member.roles.cache.has('731080197461508167')){
 
         
-        message.channel.send('sadly muh2k has yet to setup the ban finn prefix');
-        } else {
-            message.channel.send('OI whats the big idea here')
-        }
     }
  }
