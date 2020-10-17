820c18f491578cdb775b5cc038ea623c7ec0749c
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 00:13:32 +0100

    update staff 2

--- commands/STAFF.js
+++ commands/STAFF.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'staff',
     description: "STAFF",
     execute(message, args){
-        message.channel.send('staff are');
+        message.channel.send('staff members are $$paaz (the owner) muh2k (staff manager) castcodes(our professional coder) C++ (head admin) numlore (senior admin) Jokeman.Chilton(mod) Sejesteemil(mod)');
         
     }
  }
