4202418145ceadb4958c39f1c924a576176b0cfc
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 00:22:24 +0100

    updated only owner can do ban
    more coming soon

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,12 @@ module.exports = {
     name: 'ban',
     description: "this command will ban a user",
     execute(message, args){
-        message.channel.send('sadly muh2k has yet to setup the ban finn prefix');
+        if(message.member.roles.cache.has('731080197461508167')){
+
         
+        message.channel.send('sadly muh2k has yet to setup the ban finn prefix');
+        } else {
+            message.channel.send('OI whats the big idea here')
+        }
     }
  }
