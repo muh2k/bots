caba36e0741b5f0f56eb4b2007140be5c0c29d64
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 21:50:44 +0100

    attempt one at mute

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const prefix = '-';
 
+const ms = require("ms");
+
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
@@ -31,15 +33,47 @@ client.on('message', message =>{
         client.commands.get('ping').execute(message, args);
     } else if(command === 'help'){
         client.commands.get('help').execute(message, args);
-    } else if(command === 'mute'){
-        client.commands.get('mute').execute(message, args);
     } else if(command === 'wipe'){
         client.commands.get('wipe').execute(message, args);
     } else if(command === 'kill'){
         client.commands.get('kill').execute(message, args);
     } else if(command === 'staff'){
         client.commands.get('staff').execute(message, args);
-    }
+}
+switch (args[0]) {
+    case 'mute':
+        var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+        if(!person) return  message.reply("I CANT FIND THE USER " + person)
+
+        let mainrole = message.guild.roles.cache.find(role => role.name === "NORMIE");
+        let role = message.guild.roles.find(role => role.name === "Muted");
+       
+
+        if(!role) return message.reply("Couldn't find the Muted role.")
+
+
+        let time = args[2];
+        if(!time){
+            return message.reply("You didnt specify a time!");
+        }
+
+        person.addRole(mainrole.id)
+        person.removeRole(role.id);
+
+        message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
+
+        setTimeout(function(){
+            
+            person.addRole(mainrole.id)
+            person.removeRole(role.id);
+            console.log(role.id)
+            message.channel.send(`@${person.user.tag} has been unmuted.`)
+        }, ms(time));
+
+
+
+    break;
+}
 });
 
 
