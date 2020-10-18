f5b48c8c94eddd2656a3183f3746410d76cb3685
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 16:20:48 +0100

    attempt 2 at fixing bot not going up

--- commands/STAFF.js
+++ commands/STAFF.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'staff',
-    description: "STAFF",
+    description: "staff of one grid",
     execute(message, args){
         message.channel.send('staff members are $$paaz (the owner) muh2k (staff manager) castcodes(our professional coder) C++ (head admin) numlore (senior admin) Jokeman.Chilton(mod) Sejesteemil(mod)');
         
