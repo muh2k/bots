7bdc166322be2bbbd076f9aed60fd412d42aaf0b
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-18 16:37:27 +0100

    attempt 2

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ client.once('ready', () => {
 });
  
 client.on('message', message=>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
  
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
