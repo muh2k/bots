139b83a1ae0331d7bc3877e259ee29e014a46174
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-19 18:59:38 +0100

    moved mute to index

--- commands/mute.js
+++ commands/mute.js
@@ -3,43 +3,4 @@ module.exports = {
     description: "this mutes a user",
     execute(message, args){
         message.channel.send('error.MP4 muh is shit at coding and is trying to fix this')
-        
-    
-
-
-switch (args[0]) {
-    case 'mute':
-        var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
-        if(!person) return  message.reply("I CANT FIND THE USER " + person)
-
-        let mainrole = message.guild.roles.find(role => role.name === "NORMIE");
-        let role = message.guild.roles.find(role => role.name === "Muted");
-       
-
-        if(-role) return message.reply("Couldn't find the mute role.")
-
-
-        let time = args[2];
-        if(!time){
-            return message.reply("You didnt specify a time!");
-        }
-
-        person.removeRole(mainrole.id)
-        person.addRole(role.id);
-
-
-        message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
-
-        setTimeout(function(){
-            
-            person.addRole(mainrole.id)
-            person.removeRole(role.id);
-            console.log(role.id)
-            message.channel.send(`@${person.user.tag} has been unmuted.`)
-        }, ms(time));
-
-
-
-    break;
-}
     }}
--- index.js
+++ index.js
@@ -41,9 +41,45 @@ client.on('message', message =>{
         client.commands.get('staff').execute(message, args);
     } else if(command === 'mute'){
         client.commands.get('mute').execute(message, args);
+    } 
+    switch (args[0]) {
+        case 'mute':
+            var person  = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+            if(!person) return  message.reply("I CANT FIND THE USER " + person)
+    
+            let mainrole = message.guild.roles.find(role => role.name === "NORMIE");
+            let role = message.guild.roles.find(role => role.name === "Muted");
+           
+    
+            if(-role) return message.reply("Couldn't find the mute role.")
+    
+    
+            let time = args[2];
+            if(!time){
+                return message.reply("You didnt specify a time!");
+            }
+    
+            person.removeRole(mainrole.id)
+            person.addRole(role.id);
+    
+    
+            message.channel.send(`@${person.user.tag} has now been muted for ${ms(ms(time))}`)
+    
+            setTimeout(function(){
+                
+                person.addRole(mainrole.id)
+                person.removeRole(role.id);
+                console.log(role.id)
+                message.channel.send(`@${person.user.tag} has been unmuted.`)
+            }, ms(time));
+    
+    
+    
+        break;
     }
+        }
 
-});
+);
 
 
 client.login('NzU2OTM2MzIzOTMxMDQ2MDAx.X2ZF4g.wdPGZSP3NGFiqD3CRdfulH1REKQ');
