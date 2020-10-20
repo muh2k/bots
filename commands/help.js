04382286051a0b63957043ddc76339ac0257a557
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-20 15:15:27 +0100

    ads

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'help',
     description: "this commands tells you the commands of the bot",
     execute(message, args){
-        message.channel.send(' | number one is help | number two is kill | number 3 is mute | number 4 is ping | number 5 is staff | number 6 is wipe');
+        message.channel.send('| number one is help | number two is kill | number 3 is mute | number 4 is ping | number 5 is staff | number 6 is wipe');
         
     }
  }
