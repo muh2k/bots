f65145511d18bbc56d50750abacfa5979cf9225f
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-21 13:56:13 +0100

    second

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,5 @@ module.exports = {
     description: "this commands tells you the commands of the bot",
     execute(message, args){
         message.channel.send(' ```number one is help```   ```number 2 is kill```  ```number 3 is mute``` ```number 4 is ping``` ```number 5 is staff``` ```number 6 is wipe```');
-        <iframe src="why do you annoy me" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe> 
     
  }}
