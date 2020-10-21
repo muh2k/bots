9115c1691e7e4be41e8116195b9b60c11e70dbb2
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-21 14:02:44 +0100

    d

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
    name: 'ping',
    description: "this is a ping command!",
    execute(message, args){
-       message.channel.send(<iframe src="why do you annoy me" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>);
+    new Discord.MessageEmbed(hello);
        
    }
 }
