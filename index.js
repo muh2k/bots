b32f6e8cc33a5b7e7f2883bd6590d95dbb2d414b
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-25 22:39:08 +0000

    type 2

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(./commands/${file});
+
 
     client.commands.set(command.name, command);
-}
+
 
 
 client.once('ready', () => {
