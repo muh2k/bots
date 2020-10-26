dbce60c48d76e338e3c9862a8140d8b78e7871fa
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-26 18:05:35 +0000

    legit best line

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
-
+ 
     client.commands.set(command.name, command);
 }
-
 
 client.once('ready', () => {
     console.log('one grid mod!');
@@ -25,8 +24,8 @@ client.once('ready', () => {
 
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(' ');
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
