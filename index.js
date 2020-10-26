5076ba841f6f95db005255ff05d4205eb9caba82
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-26 13:38:26 +0000

    attempt 2 at fix

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const client = new Discord.Client();
 
 const prefix = '-';
 
+
+
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./commands/$,{file}`);
+    const command = require(`./commands/${file}`);
 
     client.commands.set(command.name, command);
 }
 
 
 client.once('ready', () => {
-    console.log('is now online');
+    console.log('one grid mod!');
     client.user.setActivity('over the One grid community', { type: 'WATCHING'}).catch(console.error);
 });
 
@@ -24,24 +26,26 @@ client.once('ready', () => {
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
         client.commands.get('ping').execute(message, args);
     } else if(command === 'help'){
         client.commands.get('help').execute(message, args);
-    } else if(command === 'mute'){
-        client.commands.get('mute').execute(message, args);
     } else if(command === 'wipe'){
         client.commands.get('wipe').execute(message, args);
     } else if(command === 'kill'){
         client.commands.get('kill').execute(message, args);
     } else if(command === 'staff'){
         client.commands.get('staff').execute(message, args);
-    }
-});
+    } else if(command === 'mute'){
+        client.commands.get('mute').execute(message, args);
+    } 
 
 
 
-    client.login('NzU2OTM2MzIzOTMxMDQ2MDAx.X2ZF4g.X7-KlO9aCLyNTeU05eMcIJg6wGs');
+ 
+
+
+    client.login('NzU2OTM2MzIzOTMxMDQ2MDAx.X2ZF4g.X7-KlO9aCLyNTeU05eMcIJg6wGs')});
