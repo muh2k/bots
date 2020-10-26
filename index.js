582a8c6b80761cc7d0fe50d6ccda27dedfb173fb
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-26 19:13:46 +0000

    attempt 1 at fix

--- index.js
+++ index.js
@@ -7,16 +7,36 @@ const fs = require('fs');
  
 client.commands = new Discord.Collection();
  
-
-
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
  
-
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('One grid online');
     client.user.setActivity('over the One grid community', { type: 'WATCHING'}).catch(console.error);
 });
 
+
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } else if(command === 'help'){
+        client.commands.get('help').execute(message, args);
+    } else if(command === 'wipe'){
+        client.commands.get('wipe').execute(message, args);
+    } else if(command === 'kill'){
+        client.commands.get('kill').execute(message, args);
+    } else if(command === 'staff'){
+        client.commands.get('staff').execute(message, args);
+    } else if(command === 'mute'){
+        client.commands.get('mute').execute(message, args);
+    } 
 
 
  
