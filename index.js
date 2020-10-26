e247b4fc6a006f88e047662e49d8f465c6128fd9
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-26 19:04:08 +0000

    attempt 2

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
  
 
-
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+ 
+    client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log('One grid online');
@@ -16,5 +22,10 @@ client.once('ready', () => {
 });
 
 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
-    client.login('NzU2OTM2MzIzOTMxMDQ2MDAx.X2ZF4g.X7-KlO9aCLyNTeU05eMcIJg6wGs');
+    client.login('NzU2OTM2MzIzOTMxMDQ2MDAx.X2ZF4g.X7-KlO9aCLyNTeU05eMcIJg6wGs')});
