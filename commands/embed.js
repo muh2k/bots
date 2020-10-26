0753509d8e45c5f9b08bff1f656fb08633cfd97e
muh2k <71465760+muh2k@users.noreply.github.com>
2020-10-26 19:30:23 +0000

    attempt %! at embed

---
+++ commands/embed.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'help',
+    description: "this commands tells you the commands of the bot",
+    execute(MessageEmbed, args){
+        message.channel.send(embed);
+        switch(args[0]){
+            case 'embed':
+                const embed = new Discord.MessageEmbed()
+                .addField('Player Name:', message.author.username);
+        message.channel.send(embed);
+        }
+    
+    
+    
+    }}
